3be6f245ca387b1f82e5afeadfa934faef01a803
Mohit Gawande <mohitgawande65@gmail.com>
2022-12-30 09:50:20 +0530

    created file read and show on browser

---
+++ index.js
@@ -0,0 +1,41 @@
+const dropArea = document.getElementById('drop-area');
+
+dropArea.addEventListener('dragover', (event) => {
+  event.stopPropagation();
+  event.preventDefault();
+  // Style the drag-and-drop as a "copy file" operation.
+  event.dataTransfer.dropEffect = 'copy';
+});
+
+dropArea.addEventListener('drop', (event) => {
+  event.stopPropagation();
+  event.preventDefault();
+  const fileList = event.dataTransfer.files;
+  console.log(fileList);
+  for (const file of fileList) {
+  readImage(file)
+  }
+
+});
+
+dropArea.addEventListener('change', (event) => {
+
+  const fileList = event.target.files;
+  console.log(fileList);
+});
+
+
+const img =document.getElementById('show-area');
+function readImage(file) {
+  // Check if the file is an image.
+  if (file.type && !file.type.startsWith('image/')) {
+    console.log('File is not an image.', file.type, file);
+    return;
+  }
+
+  const reader = new FileReader();
+  reader.addEventListener('load', (event) => {
+    img.src = event.target.result;
+  });
+  reader.readAsDataURL(file);
+}
